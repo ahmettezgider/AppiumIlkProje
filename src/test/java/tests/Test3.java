24c03d68c7aef5be5072d7fa89ea1a22b9d22b2a
atezgider <atezgider@gmail.com>
2022-07-21 21:09:52 +0200

    Initial commit

---
+++ src/test/java/tests/Test3.java
@@ -0,0 +1,60 @@
+package tests;
+
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import utils.App;
+import utils.Device;
+
+import java.net.MalformedURLException;
+
+public class Test3 {
+
+    AppiumDriverLocalService service;
+
+    Device device = Device.PIXEL2;
+    App app = App.APIDEMO;
+
+    @Test
+    public void test1() throws MalformedURLException {
+
+        service = new AppiumServiceBuilder()
+                .withIPAddress("127.0.0.1")
+                //.usingPort(1111)
+                .usingAnyFreePort()
+                .build();
+        service.start();
+
+
+
+        // Desired capability
+        // hangi cihaz ve hangi uygulamaya baglanilacak
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("appium:udid", device.udid);
+        capabilities.setCapability("appium:version", device.version);
+        capabilities.setCapability("appium:deviceName", device.deviceName);
+        capabilities.setCapability("platformName", device.platformName);
+
+        capabilities.setCapability("appium:appPackage", app.appPackage);
+        capabilities.setCapability("appium:appActivity", app.appActivity);
+
+        // RemoteWebDriver -> WebDriver -> AppiumDriver --> (AndroidDriver, iOSDriver)
+
+        AppiumDriver<MobileElement> driver;
+
+        driver = new AndroidDriver<>(service.getUrl(), capabilities);
+
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='Accessibility']")).click();
+
+        driver.closeApp();
+        service.stop();
+
+    }
+
+}
